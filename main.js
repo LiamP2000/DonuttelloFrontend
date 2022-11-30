5e466604929af3fd3030deaeae615e509a65c161
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-11-30 13:08:51 +0100

    build: zet glazuur data in URL

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-var form = document.getElementById("nextTopping");
-function handleForm(event) { event.preventDefault(); } 
-form.addEventListener('submit', handleForm);
+let toppings = [];
 
-/*check if button with ID nextTopping got clicked*/
-console.log("main.js loaded");
-document.getElementById("nextTopping").onclick = function() {
-    /*if yes, then get the value of the input field with ID topping*/
-    var topping = document.getElementById("topping").value;
-    /*and add it to the list with ID toppings*/
-    document.getElementById("toppings").innerHTML += "<li>" + topping + "</li>";
-    /*and clear the input field*/
-    document.getElementById("topping").value = "";
-    console.log(topping);
+/**get data from URL */
+function getToppings() {
+    let url = new URL(window.location.href);
+    let searchParams = new URLSearchParams(url.search);
+    /*add all 'glazuur' to toppings array */
+    for (let param of searchParams) {
+        if (param[0] === 'glazuur') {
+            toppings.push(param[1]);
+        }
+    }
 }
+if(getToppings() === undefined){
+    console.log(toppings);
+}
+else{
+    console.log('no toppings');
+}
+
+
