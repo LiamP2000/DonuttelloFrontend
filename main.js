ff949d017ad67b9abf35f4e2022125bf1046e0f3
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-11-30 12:15:23 +0100

    fix: verander topping naar glazuur

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+var form = document.getElementById("nextTopping");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
 
+/*check if button with ID nextTopping got clicked*/
+console.log("main.js loaded");
+document.getElementById("nextTopping").onclick = function() {
+    /*if yes, then get the value of the input field with ID topping*/
+    var topping = document.getElementById("topping").value;
+    /*and add it to the list with ID toppings*/
+    document.getElementById("toppings").innerHTML += "<li>" + topping + "</li>";
+    /*and clear the input field*/
+    document.getElementById("topping").value = "";
+    console.log(topping);
+}
