608bf9a93baafc693f910d3ab6dd1c6bb2ae8992
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-12-07 11:02:54 +0100

    Build: add donut to scene

--- public/js/three.js
+++ public/js/three.js
@@ -1,6 +1,5 @@
-import './style.css'
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from '../../node_modules/three/build/three.module.js';
+import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 const donutCanvas = document.getElementById('app');
 const renderer = new THREE.WebGLRenderer({canvas: donutCanvas});
@@ -8,22 +7,41 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, donutCanvas.clientWidth / donutCanvas.clientHeight, 0.1, 1000);
 
+/** make scene and canvas see through*/
+scene.background = null;
+
+/** ambient light */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+
 /** add light */
 const light = new THREE.PointLight(0xffffff, 1, 1000);
-light.position.set(0, 0, 0);
+light.position.set(0.2, 0.3, 0.5);
 scene.add(light);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+/** load glb file from ../models/donut.glb */
+const loader = new GLTFLoader();
+loader.load('../models/donut.glb', function (gltf) {
+    scene.add(gltf.scene);
+}, undefined, function (error) {
+    console.error(error);
+});
 
-camera.position.z = 5;
+/** set camera position */
+camera.position.z = 0.5;
+camera.position.y = 0.3;
+/**aim camera at center */
+camera.lookAt(0, 0, 0);
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    /**rotate donut on an steady angle */
+    
+
+    scene.rotation.y += 0.001;
+
+    /**rotate cube */
     renderer.render(scene, camera);
 }
 
