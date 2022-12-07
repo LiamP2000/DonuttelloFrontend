7a0b281b0a375f0ad8d391ce91269485cc5bb14f
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-12-07 11:13:00 +0100

    fix: put threejs canvas in place

--- public/js/three.js
+++ public/js/three.js
@@ -3,11 +3,13 @@ import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLo
 
 const donutCanvas = document.getElementById('app');
 const renderer = new THREE.WebGLRenderer({canvas: donutCanvas});
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth/10*4, window.innerHeight/2-50 );
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, donutCanvas.clientWidth / donutCanvas.clientHeight, 0.1, 1000);
 
 /** make scene and canvas see through*/
+
+renderer.setClearColor(0x000000, 0);
 scene.background = null;
 
 /** ambient light */
@@ -28,17 +30,21 @@ loader.load('../models/donut.glb', function (gltf) {
     console.error(error);
 });
 
+
+/** get layers of donut.glb */
+
+const donut = scene.getObjectByName('donut');
+console.log(donut);
+
 /** set camera position */
-camera.position.z = 0.5;
-camera.position.y = 0.3;
+camera.position.z = 0.14;
+camera.position.y = 0.12;
 /**aim camera at center */
 camera.lookAt(0, 0, 0);
 
 function animate() {
     requestAnimationFrame(animate);
-    /**rotate donut on an steady angle */
-    
-
+    /**rotate donut */
     scene.rotation.y += 0.001;
 
     /**rotate cube */
