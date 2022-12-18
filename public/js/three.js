26be73199ff5959255ad276a0d0ae510295c4d15
JanaClaessens <janaclaessens@icloud.com>
2022-12-18 18:26:26 +0100

    fix: Fix threejs import

--- public/js/three.js
+++ public/js/three.js
@@ -1,6 +1,5 @@
-import * as THREE from '../../node_modules/three/build/three.module.js';
-import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
 const donutCanvas = document.getElementById('app');
 const renderer = new THREE.WebGLRenderer({canvas: donutCanvas});
